ff9e50364d871b5d10c270d2d027d2d161bc69db
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-10-21 16:22:02 +0500

    chnages

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname)
+
+const config = {
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    resolver: {
+        extraNodeModules: {
+            ...require("node-libs-react-native"),
+        },
+    },
+}
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
